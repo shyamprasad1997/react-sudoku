272944b7b44caee9519ae62dbe5cd7c1d1dab40d
jasshopper@gmail.com <Terminator808>
2021-08-28 21:10:32 +0530

    Updates

---
+++ precache-manifest.16dd9274b48d1d4f89c3ddd0fa5564a5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9656cb1bd3ce79efd94f77d6d6be2d6f",
+    "url": "/react-sudoku/index.html"
+  },
+  {
+    "revision": "3e9e087fa730e194478b",
+    "url": "/react-sudoku/static/css/main.5ea30d3a.chunk.css"
+  },
+  {
+    "revision": "a96dff82916908074592",
+    "url": "/react-sudoku/static/js/2.dd0121fb.chunk.js"
+  },
+  {
+    "revision": "1b9cc396521eecb2045c787139670fba",
+    "url": "/react-sudoku/static/js/2.dd0121fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e9e087fa730e194478b",
+    "url": "/react-sudoku/static/js/main.fc0b2cb6.chunk.js"
+  },
+  {
+    "revision": "c19f8a563084def56f10",
+    "url": "/react-sudoku/static/js/runtime-main.355a75d5.js"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/react-sudoku/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/react-sudoku/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/react-sudoku/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/react-sudoku/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/react-sudoku/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/react-sudoku/static/media/square1.ca257073.png"
+  },
+  {
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "/react-sudoku/static/media/square2.6619b979.png"
+  },
+  {
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "/react-sudoku/static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/react-sudoku/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/react-sudoku/static/media/square5.ff004a0e.png"
+  }
+]);
