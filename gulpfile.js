76a78ff135da81ed1bafd63017d13561450eb9b3
jasshopper@gmail.com <Terminator808>
2021-08-28 21:44:25 +0530

    change gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,28 +4,13 @@ const gap = require("gulp-append-prepend");
 gulp.task("licenses", async function() {
   gulp
     .src("build/static/js/*chunk.js", { base: "./" })
-    .pipe(
-      gap.prependText(
-        `/*! * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.*/`
-      )
-    )
     .pipe(gulp.dest("./", { overwrite: true }));
 
   gulp
     .src("build/index.html", { base: "./" })
-    .pipe(
-      gap.prependText(`<!--
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
--->`)
-    )
     .pipe(gulp.dest("./", { overwrite: true }));
   gulp
     .src("build/static/css/*chunk.css", { base: "./" })
-    .pipe(
-      gap.prependText(`/*!
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/`)
-    )
     .pipe(gulp.dest("./", { overwrite: true }));
   return;
 });
